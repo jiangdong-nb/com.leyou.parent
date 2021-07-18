d486f670f6d02d3e39c6d9a226c3579170bee351
JD_clarence <JD_clarence@163.com>
2021-07-18 17:19:20 +0800

    新增商品品牌查询 新增公共分页类

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/BrandController.java
@@ -0,0 +1,47 @@
+package com.leyou.item.controller;
+
+import com.leyou.commom.pojo.PageResult;
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("brand")
+public class BrandController {
+    @Autowired
+    private BrandService brandService;
+
+    /**
+     *
+     * 根据查询条件分页返回查询结果
+     * @param key
+     * @param page
+     * @param rows
+     * @param sortBy
+     * @param desc
+     * @return
+     */
+    @GetMapping("page")
+    public ResponseEntity<PageResult<Brand>> queryBrandsByPage(
+            @RequestParam(value = "key",required = false)String key,
+            @RequestParam(value = "page",defaultValue = "1")Integer page,
+            @RequestParam(value = "rows",defaultValue = "5")Integer rows,
+            @RequestParam(value = "sortBy",required = false)String sortBy,
+            @RequestParam(value = "desc",required = false)Boolean desc
+    ){
+        PageResult<Brand> result = this.brandService.queryBrandsByPage(key,page,rows,sortBy,desc);
+        if(CollectionUtils.isEmpty(result.getItems())){
+            System.out.println("1");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(result);
+    }
+
+}
